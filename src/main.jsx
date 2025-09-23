af27621113bbd68d3b9c390ea4b2d7875f90e819
Alan Clarke <alannoelc@gmail.com>
2025-09-23 23:49:35 +0100

    Deploy to GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from "./App";
 import { AppStoreProvider } from "./context/AppStore";
 import "./index.css";
 
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL)
+console.log("Supabase Key present?:", !!import.meta.env.VITE_SUPABASE_ANON_KEY)
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppStoreProvider>
